0037688b032e6c1ed98baa55b2a84140baff5af4
praveennegi420 <praveennegiuk07@gmail.com>
2023-03-18 19:07:27 +0530

    client updated

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const authRoutes = require("./routes/auth");
 const songRoutes = require("./routes/songs");
 const playListRoutes = require("./routes/playLists");
 const searchRoutes = require("./routes/search");
-const clientRoutes = require('./routes/client')
 const app = express();
 
 connection();
@@ -22,27 +21,15 @@ app.use("/api/login/", authRoutes);
 app.use("/api/songs/", songRoutes);
 app.use("/api/playlists/", playListRoutes);
 app.use("/api/", searchRoutes);
-// app.use("/", clientRoutes);
 
-app.get('/css-initial', (req,res)=>{
-    res.sendFile(path.resolve(__dirname+'/build/static/css/2.b28305bc.chunk.css'))
-})
+// CLIENT ROUTES FOR VERCEL
+app.get('/css-initial', (req,res)=>{res.sendFile(path.resolve(__dirname+'/build/static/css/2.b28305bc.chunk.css'))})
+app.get('/css-final', (req,res)=>{res.sendFile(path.resolve(__dirname+'/build/static/css/main.b1eed2c3.chunk.css'))})
+app.get('/js-inital', (req,res)=>{res.sendFile(path.resolve(__dirname+'/build/static/js/2.9de7726f.chunk.js'))})
+app.get('/js-final', (req,res)=>{res.sendFile(path.resolve(__dirname+'/build/static/js/main.f052993f.chunk.js'))})
+app.get('/icon', (req,res)=>{ res.sendFile(path.resolve(__dirname+ '/build/favicon.png'))})
+app.get('*', (req, res) => { res.sendFile(path.resolve(__dirname + '/index.html')) })  
 
-app.get('/css-final', (req,res)=>{
-    res.sendFile(path.resolve(__dirname+'/build/static/css/main.b1eed2c3.chunk.css'))
-})
-app.get('/js-inital', (req,res)=>{
-    res.sendFile(path.resolve(__dirname+'/build/static/js/2.9de7726f.chunk.js'))
-})
-app.get('/js-final', (req,res)=>{
-    res.sendFile(path.resolve(__dirname+'/build/static/js/main.f052993f.chunk.js'))
-})
-app.get('/icon', (req,res)=>{
-    res.sendFile(path.resolve(__dirname+'/build/favicon.png'))
-})
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/index.html'))
-})  
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
