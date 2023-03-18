1c5d3113ce832926974bd040496975bc3cf02280
praveennegi420 <praveennegiuk07@gmail.com>
2023-03-18 16:35:29 +0530

    client added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const cors = require("cors");
+const path = require('path')
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
@@ -13,12 +14,17 @@ const app = express();
 connection();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./build'))
 
 app.use("/api/users/", userRoutes);
 app.use("/api/login/", authRoutes); 
 app.use("/api/songs/", songRoutes);
 app.use("/api/playlists/", playListRoutes);
 app.use("/api/", searchRoutes);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/build/index.html'))
+})
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
